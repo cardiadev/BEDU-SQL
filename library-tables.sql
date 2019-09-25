41587c95dc183cb17a7c51cb4d230dd07aec2ed3
Carlos Diaz <hola@cjdiaz.com>
2019-09-24 20:24:22 -0500

    Creating a Query to INSERT data on table books

--- library-tables.sql
+++ library-tables.sql
@@ -6,3 +6,8 @@ CREATE TABLE books (
   stock INT NOT NULL,
   PRIMARY KEY (bookid)
 );
+
+INSERT INTO books (title, author, published, stock)
+VALUES ('Como Programar Volumen 1', 'Raul Rios', '2015-08-24',50),
+       ('Hija de humo y hueso', 'Valeria Medina', '2011-09-23',5),
+       ('El Jardin Perdido', 'Kate Austen', '2014-04-03',150);
